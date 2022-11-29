1458443d5d8418bc3e7fe9f7ba2eb1d521fc497c
lyl <1552412358@qq.com>
2022-11-29 19:56:25 +0800

    first idea git commit

---
+++ Git-test/src/main/java/com/lyl/gittest/GitTest.java
@@ -0,0 +1,15 @@
+package com.lyl.gittest;
+
+/**
+ * 功能：
+ *
+ * @author 林亦亮
+ * @version 1.0
+ * @date 2022/11/29
+ */
+
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+	}
+}
